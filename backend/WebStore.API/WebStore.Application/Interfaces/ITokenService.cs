8f7b9aa009c174facead83f5ad399882ac02e83c
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-02-22 14:49:37 -0300

    feat: add JWT auth

---
+++ backend/WebStore.API/WebStore.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebStore.API.Interfaces;
+
+public interface ITokenService
+{
+    public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);
+    public string GenerateRefreshToken();
+    public ClaimsPrincipal getPrincipalFromExpiredToken(string token, IConfiguration _config);
+}
---
+++ backend/WebStore.API/WebStore.Application/Services/TokenService.cs
@@ -0,0 +1,24 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WebStore.API.Interfaces;
+
+namespace WebStore.API.Services;
+
+public class TokenService : ITokenService
+{
+    
+    public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateRefreshToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ClaimsPrincipal getPrincipalFromExpiredToken(string token, IConfiguration _config)
+    {
+        throw new NotImplementedException();
+    }
+}
