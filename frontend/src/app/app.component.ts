b63568c97c2ecfe04158f2092073e5152b426916
Augusto <augusto.medeirosdacosta@gmail.com>
2024-03-03 21:15:52 -0300

    feat: make sidebar fully responsive and create item details routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {AsyncPipe} from "@angular/common";
 import {SideBarComponent} from "./components/side-bar/side-bar.component";
+import {ItemDetailsComponent} from "./components/item-details/item-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AsyncPipe, RouterLink, SideBarComponent],
+  imports: [RouterOutlet, HomeComponent, AsyncPipe, RouterLink, SideBarComponent,ItemDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas : [CUSTOM_ELEMENTS_SCHEMA]
--- frontend/src/app/components/item-card/item-card.component.ts
+++ frontend/src/app/components/item-card/item-card.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'item-card-app',
   templateUrl: 'item-card.component.html',
   styleUrl: 'item-card.component.css',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterLink],
 })
 export class ItemCardComponent {}
