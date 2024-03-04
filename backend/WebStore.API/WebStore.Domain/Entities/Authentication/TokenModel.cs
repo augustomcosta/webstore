998e7cfc830ffb4d8f628de3ff48b1e349ceb5cd
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-03-04 15:17:43 -0300

    feat: add token model

---
+++ backend/WebStore.API/WebStore.Domain/Entities/Authentication/TokenModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebStore.Domain.Entities.Authentication;
+
+public sealed class TokenModel
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
