2e939af87fb1d811d82d37ede5a6d997a12d1ae1
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-04-10 15:10:56 -0300

    feat: implement add item to basket function

--- frontend/src/app/components/item-details/item-details.component.ts
+++ frontend/src/app/components/item-details/item-details.component.ts
@@ -4,7 +4,8 @@ import { IProduct } from '../../core/models/IProduct';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BasketSummaryComponent } from '../basket-summary/basket-summary.component';
 import { BasketTotalsComponent } from '../basket-totals/basket-totals.component';
-import { NgOptimizedImage } from '@angular/common';
+import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
+import { BasketService } from '../../services/basket.service';
 
 @Component({
   selector: 'app-item-details',
@@ -14,6 +15,7 @@ import { NgOptimizedImage } from '@angular/common';
     BasketTotalsComponent,
     RouterLink,
     NgOptimizedImage,
+    CurrencyPipe,
   ],
   templateUrl: './item-details.component.html',
   styleUrl: './item-details.component.css',
@@ -23,8 +25,11 @@ export class ItemDetailsComponent implements OnInit {
   // @ts-ignore
   product: IProduct;
   activatedRoute = inject(ActivatedRoute);
+  basketService = inject(BasketService);
 
-  // @ts-ignore
+  addItemToBasket(item: IProduct) {
+    this.basketService.addItemToBasket(item);
+  }
 
   getProductById() {
     const productId = this.activatedRoute.snapshot.paramMap.get('id');
