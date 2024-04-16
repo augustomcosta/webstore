e0b296ae46263cfe35827c209dcc0934c6014aef
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-04-16 08:56:16 -0300

    feat: add new seed data and images

---
+++ frontend/src/app/services/category.service.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {ICategory} from "../core/models/category";
+import {BehaviorSubject, Observable, tap} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  http = inject(HttpClient);
+  apiUrl = environment.apiUrl;
+  categoriesSource = new BehaviorSubject<ICategory[]>(null as any);
+  categories$ = this.categoriesSource.asObservable();
+
+  constructor() { }
+
+  getCategories() {
+    return this.http.get<ICategory[]>(this.apiUrl + `/Category`,).
+    pipe(
+      tap((categories: ICategory[]) => {
+      this.categoriesSource.next(categories);
+    }));
+  }
+}
