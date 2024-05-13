54297f50a7dee02b17270aff06f6f1ba5a03d426
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-05-13 15:00:23 -0300

    feat: create delivery method service and entity

---
+++ frontend/src/app/core/models/delivery-method.ts
@@ -0,0 +1,7 @@
+export interface IDeliveryMethod {
+  id: string;
+  name: string;
+  deliveryTime: string;
+  description: string;
+  price: number;
+}
---
+++ frontend/src/app/services/delivery-method.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { BehaviorSubject, tap } from 'rxjs';
+import { IDeliveryMethod } from '../core/models/delivery-method';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DeliveryMethodService {
+  http = inject(HttpClient);
+  apiUrl = environment.apiUrl;
+  deliveryMethodsSource = new BehaviorSubject<IDeliveryMethod[]>(null as any);
+  deliveryMethods$ = this.deliveryMethodsSource.asObservable();
+
+  constructor() {}
+
+  getDeliveryMethods() {
+    return this.http
+      .get<IDeliveryMethod[]>(this.apiUrl + `/DeliveryMethod`)
+      .pipe(
+        tap((deliveryMethods: IDeliveryMethod[]) => {
+          this.deliveryMethodsSource.next(deliveryMethods);
+        }),
+      );
+  }
+}
