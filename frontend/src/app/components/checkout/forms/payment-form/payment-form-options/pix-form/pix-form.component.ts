d6f46832a6faeb9d57cca2f5d65b653ed015c6da
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-05-13 15:01:49 -0300

    feat: add qr-code generation to pix payment

--- frontend/src/app/components/checkout/forms/payment-form/payment-form-options/pix-form/pix-form.component.ts
+++ frontend/src/app/components/checkout/forms/payment-form/payment-form-options/pix-form/pix-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-pix-form',
   standalone: true,
-  imports: [],
+  imports: [QRCodeModule],
   templateUrl: './pix-form.component.html',
-  styleUrl: './pix-form.component.css'
+  styleUrl: './pix-form.component.css',
 })
 export class PixFormComponent {
-
+  pixQrCode =
+    'https://audaces.com/pt-br/solucoes/audaces-360?gad_source=1&gclid=EAIaIQobChMI-oqy5fyKhgMV0jjUAR18bgN5EAAYASAAEgJnn_D_BwE';
 }
