2ae45c007fcb702e1c4513c21c4020da5c699155
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-05-14 14:53:06 -0300

    feat: add order totals and item details

--- frontend/src/app/components/checkout/order-summary/order-summary.component.ts
+++ frontend/src/app/components/checkout/order-summary/order-summary.component.ts
@@ -1,14 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as CheckoutActions from '../data/checkout.actions';
 import { BasketService } from '../../../services/basket.service';
 import { Observable } from 'rxjs';
-import { IBasket } from '../../../core/models/basket';
+import { IBasket, IBasketTotals } from '../../../core/models/basket';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { IPaymentMethod } from '../../../core/models/payment-method';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, AsyncPipe, RouterLink],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css',
 })
@@ -17,10 +20,10 @@ export class OrderSummaryComponent implements OnInit {
   basketService = inject(BasketService);
   basket$: Observable<IBasket> | undefined;
   basket!: IBasket;
+  basketTotal$: Observable<IBasketTotals> | undefined;
+  @Input() paymentMethod!: IPaymentMethod;
 
-  previousStep() {
-    this.store.dispatch(CheckoutActions.previousStep());
-  }
+  constructor() {}
 
   getBasket() {
     this.basketService.getBasketFromLoggedUser().subscribe((basket) => {
@@ -28,8 +31,13 @@ export class OrderSummaryComponent implements OnInit {
     });
   }
 
+  previousStep() {
+    this.store.dispatch(CheckoutActions.previousStep());
+  }
+
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.basketTotal$ = this.basketService.basketTotal$;
     this.getBasket();
   }
 }
