69ef59be20dcbdbe77bb9eeebb74ee8123db7ad1
Augusto <augusto.medeirosdacosta@gmail.com>
2024-05-23 13:52:37 -0300

    minor: changes to orders component template

--- frontend/src/app/components/orders/orders.component.ts
+++ frontend/src/app/components/orders/orders.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { BasketSummaryComponent } from '../basket/basket-summary/basket-summary.component';
 import { BasketTotalsComponent } from '../basket/basket-totals/basket-totals.component';
 import { RouterLink } from '@angular/router';
 import { OrderService } from '../../services/order.service';
 import { Observable } from 'rxjs';
 import { Order } from '../../core/models/order';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @Component({
   selector: 'app-orders',
@@ -15,6 +16,9 @@ import { Order } from '../../core/models/order';
     BasketSummaryComponent,
     BasketTotalsComponent,
     RouterLink,
+    OrderDetailsComponent,
+    CurrencyPipe,
+    NgOptimizedImage,
   ],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css',
@@ -22,6 +26,7 @@ import { Order } from '../../core/models/order';
 export class OrdersComponent implements OnInit {
   orderService = inject(OrderService);
   userOrders$!: Observable<Order[]>;
+  orders!: Order[];
 
   ngOnInit(): void {
     this.orderService.getAllOrdersForUser();
